94732df339e169e5d79777bff3f61ee5b82c91a5
isaacc01 <isaacalejandro07@gmail.com>
2023-07-15 13:17:13 -0700

    finalcommit

--- src/components/ArtistView.js
+++ src/components/ArtistView.js
@@ -1,8 +1,15 @@
-function ArtistView() {
+import { useParams } from 'react-router-dom'
 
-   return (
-    <h1>Code from Code Along coming soon</h1>
-   )
+function ArtistView() {
+    const { id } = useParams()
+    const [ artistData, setArtistData ] = useState([])
+
+    return (
+        <div>
+            <h2>The id passed was: {id}</h2>
+            <p>Artist Data Goes Here!</p>
+        </div>
+    )
 }
 
 export default ArtistView
